ea23e1f06b41f15da5a89464e807bdee5a0c41ae
PeytenBevill <redsage00@gmail.com>
2024-03-07 16:39:58 -0600

    realized I got a user using the wrong id

--- routes/user.js
+++ routes/user.js
@@ -2,7 +2,7 @@ const express = require("express");
 const userController = require("../controllers/users.controller");
 const router = express.Router();
 
-router.get("/:_id", userController.getUser);
+router.get("/:userID", userController.getUser);
 router.post("/", userController.create);
 
 module.exports = router;
